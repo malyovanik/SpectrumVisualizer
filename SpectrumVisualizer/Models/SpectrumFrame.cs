5e48cba7175d1bae3df1e7eda8775f46393cb26d
DESKTOP-9VOSS81\Malyovanik <malyovanik.konstantin@gmail.com>
2026-07-06 00:16:00 +0300

    #5: Implemented WaterfallHistory + refactoring

---
+++ SpectrumVisualizer/Models/SpectrumFrame.cs
@@ -0,0 +1,3 @@
+﻿namespace SpectrumVisualizer.Models;
+
+public record SpectrumFrame(double[] Powers, double[] Frequencies, DateTime Timestamp);
---
+++ SpectrumVisualizer/Models/WaterfallHistory.cs
@@ -0,0 +1,28 @@
+﻿namespace SpectrumVisualizer.Models;
+
+public class WaterfallHistory(int capacity)
+{
+    private readonly SpectrumFrame?[] _buffer = new SpectrumFrame?[capacity];
+    private readonly int _capacity = capacity;
+    private int _head;
+    private int _countWritedFrames;
+
+    public void AddFrame(SpectrumFrame frame)
+    {
+        _buffer[_head] = frame;
+        if (_countWritedFrames < _capacity) {
+            _countWritedFrames++;
+        }
+        _head = (_head + 1) % _capacity;
+    }
+
+    public SpectrumFrame? GetFrame(int index)
+    {
+        if (index < 0 || index >= _countWritedFrames)
+            return null;
+
+        return _buffer[(_head - 1 - index + _capacity) % _capacity];
+    }
+
+    public int CountWritedFrames => _countWritedFrames;
+}
